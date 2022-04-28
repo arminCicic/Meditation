6181fe4c87f8002775c1f20d67ccc9686d33ef3a
Armin Cicic <armin.cicic@mvteo.gov.ba>
2022-04-28 15:46:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector("#navigation-container")
+
+
+
+window.addEventListener("scroll", () => {
+    console.log("scrolling")
+    nav.classList.add("on-scroll-nav")
+})
+   
