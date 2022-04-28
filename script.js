3a0dc4c13257e2c43b3646fdafe467e6c8a31551
arminCicic <armincicic@gmail.com>
2022-04-28 23:33:59 +0200

    added the about us page

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const nav = document.querySelector("#navigation-container")
-
-
+const body = document.body;
+const nav = document.querySelector("#navigation-container");
+const icon = document.querySelector("#icon");
+const links = document.querySelectorAll(".links");
 
 window.addEventListener("scroll", () => {
-    console.log("scrolling")
-    nav.classList.add("on-scroll-nav")
-})
-   
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    nav.classList.add("on-scroll-nav");
+    icon.classList.add("on-scroll-nav-text");
+    links.forEach((link) => link.classList.add("on-scroll-nav-text"));
+  } else {
+    nav.classList.remove("on-scroll-nav");
+    icon.classList.remove("on-scroll-nav-text");
+    links.forEach((link) => link.classList.remove("on-scroll-nav-text"));
+  }
+});
