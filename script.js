fd80110cf4c8f727aca99219b206aa5918bf4601
arminCicic <armincicic@gmail.com>
2022-05-05 23:00:29 +0200

    first draft of app

--- script.js
+++ script.js
@@ -5,14 +5,39 @@ const links = document.querySelectorAll(".links");
 const tryForFree = document.querySelector("#try-for-free");
 const menu = document.querySelector(".menu");
 const hamburgerMenuIcon = document.querySelector(".hamburger-menu");
+const closeIcon = document.querySelector(".close-icon");
+const barsIcon = document.querySelector(".bars-icon");
 // const button = document.querySelector("#button");
 
-hamburgerMenuIcon.addEventListener("click", () => {
-  menu.classList.remove("menu");
-  menu.classList.add("menu-responsive");
+let menuShow = true;
+
+// Event listener
+
+// Toggle bars and close icon, make menu responsive
+barsIcon.addEventListener("click", () => {
+  if (menuShow) {
+    barsIcon.style.display = "none";
+    closeIcon.style.display = "block";
+    menu.classList.remove("menu");
+    menu.classList.add("menu-responsive");
+  }
 });
 
-// Event listeners
+// Toggle close button and menu class on nav element
+closeIcon.addEventListener("click", () => {
+  barsIcon.style.display = "block";
+  closeIcon.style.display = "none";
+  menu.classList.add("menu");
+});
+
+// Revert menu-responsive class on window resize
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    menu.classList.remove("menu-responsive");
+  } else {
+    menu.classList.add("menu-responsive");
+  }
+});
 
 //Redirect page on clik and using local storage to send id of audio to playAudio.js
 button1.addEventListener("click", () => {
